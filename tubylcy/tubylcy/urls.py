f75ec92c7b4207204a33a8b398b5be8ff1e34e42
Kamil Gałuszka <kgaluszka@nglogic.com>
2014-07-12 21:00:55 +0200

    using {% static %}

--- tubylcy/tubylcy/urls.py
+++ tubylcy/tubylcy/urls.py
@@ -1,5 +1,9 @@
+import sys
+from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import patterns, url, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework import routers
 from locations import views
 
@@ -18,4 +22,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
