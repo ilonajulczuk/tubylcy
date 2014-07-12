de68fd5a75168d0b8a688d2f865a5c1364288238
pablo-the-programmer <pablo@hackerspace.pl>
2014-07-12 15:22:22 +0200

    add views and landing page

--- tubylcy/locations/urls.py
+++ tubylcy/locations/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-admin.autodiscover()
+from locations import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tubylcy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', include('locations.urls')),
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- tubylcy/locations/views.py
+++ tubylcy/locations/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template import RequestContext, loader
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('locations/index.html')
+    context = RequestContext(request, {
+        #here shit from *models* useful for webpage
+    })
+    return HttpResponse(template.render(context))
+
